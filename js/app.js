d5b2f9d224f126ee7634c86b63ed40310eb4bb56
Garima Kamboj <garima@koove.com>
2016-10-26 16:10:40 +0530

    Result shown except graph

--- js/app.js
+++ js/app.js
@@ -1,33 +1,6 @@
 var myApp = angular.module('myApp', ['ngMessages', 'ngResource', 'ngRoute']);
 
-var questions = [
-  {
-    'statement': 'Which is your fav pet?',
-    'options': ['http://placehold.it/300/text=garima+rocks', 'http://placehold.it/300/text=garima+lols', 'http://placehold.it/300/text=garima+walks'],
-    'ans': 'http://placehold.it/300/text=garima+rocks'
-  },
-  {
-    'statement': 'What is your favourite color?',
-    'options': ['http://placehold.it/300/text=garima+rocks', 'http://placehold.it/300/text=garima+lols', 'http://placehold.it/300/text=garima+walks'],
-    'ans': 'http://placehold.it/300/text=garima+rocks'
-  },
-  {
-    'statement': 'What is your hobby?',
-    'options': ['http://placehold.it/300/text=garima+rocks', 'http://placehold.it/300/text=garima+lols', 'http://placehold.it/300/text=garima+walks'],
-    'ans': 'http://placehold.it/300/text=garima+rocks'
-  },
-  {
-    'statement': 'In which language do you like to program?',
-    'options': ['http://placehold.it/300/text=garima+rocks', 'http://placehold.it/300/text=garima+lols', 'http://placehold.it/300/text=garima+walks'],
-    'ans': 'http://placehold.it/300/text=garima+rocks'
-  },
-  {
-    'statement': 'Which is your favourite company?',
-    'options': ['http://placehold.it/300/text=garima+rocks', 'http://placehold.it/300/text=garima+lols', 'http://placehold.it/300/text=garima+walks'],
-    'ans': 'http://placehold.it/300/text=garima+rocks'
-  }
 
-];
 
 myApp.config(function($routeProvider) {
   $routeProvider
@@ -38,26 +11,67 @@ myApp.config(function($routeProvider) {
 });
 
 myApp.controller('appController', ['$scope', function($scope) {
+  $scope.questions = [
+    {
+      'statement': 'Which is your fav pet?',
+      'options': ['http://placehold.it/300/text=garima+rocks', 'http://placehold.it/300/text=garima+lols', 'http://placehold.it/300/text=garima+walks'],
+      'ans': 'http://placehold.it/300/text=garima+rocks'
+    },
+    {
+      'statement': 'What is your favourite color?',
+      'options': ['http://placehold.it/300/text=garima+rocks', 'http://placehold.it/300/text=garima+lols', 'http://placehold.it/300/text=garima+walks'],
+      'ans': 'http://placehold.it/300/text=garima+rocks'
+    },
+    {
+      'statement': 'What is your hobby?',
+      'options': ['http://placehold.it/300/text=garima+rocks', 'http://placehold.it/300/text=garima+lols', 'http://placehold.it/300/text=garima+walks'],
+      'ans': 'http://placehold.it/300/text=garima+rocks'
+    },
+    {
+      'statement': 'In which language do you like to program?',
+      'options': ['http://placehold.it/300/text=garima+rocks', 'http://placehold.it/300/text=garima+lols', 'http://placehold.it/300/text=garima+walks'],
+      'ans': 'http://placehold.it/300/text=garima+rocks'
+    },
+    {
+      'statement': 'Which is your favourite company?',
+      'options': ['http://placehold.it/300/text=garima+rocks', 'http://placehold.it/300/text=garima+lols', 'http://placehold.it/300/text=garima+walks'],
+      'ans': 'http://placehold.it/300/text=garima+rocks'
+    }
+
+  ];
   $scope.result = 'dogs';
   var currentQuestionNumber = 0;
-  $scope.currentQuestion = questions[currentQuestionNumber];
-
+  $scope.currentQuestion = $scope.questions[currentQuestionNumber];
+  $scope.correct = [];
+  $scope.incorrect = [];
   $scope.next = function() {
-    ++currentQuestionNumber
-    if(currentQuestionNumber < questions.length) {
-      $scope.currentQuestion = questions[currentQuestionNumber];
+
+    $scope.currentQuestion.userAnswer = $scope.result;
+
+    if($scope.currentQuestion.ans === $scope.result) {
+      $scope.correct.push($scope.result);
+    }
+    else {
+      $scope.incorrect.push($scope.result);
+    };
+    if(currentQuestionNumber < $scope.questions.length) {
+      $scope.currentQuestion = $scope.questions[currentQuestionNumber];
       $('#qlist').hide();
     }
     else {
+      $('#quest').hide();
       $('#qlist').show();
-    }
-  };
+    };
+    ++currentQuestionNumber;
 
+  };
+  $('#qlist').hide();
   $scope.name = '';
   $scope.nameArr = [];
   $scope.clicked = function() {
     $scope.nameArr.push($scope.name);
     $('label').hide();
+
   };
 
 }]);
